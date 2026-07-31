bee0d88afd4c8eb1c7aaab81765d3dd6c6c45413
Sacha <s.ruhe@atabix.nl>
2026-07-31 13:55:57 +0200

    update awsssh

--- Casks/awsssh.rb
+++ Casks/awsssh.rb
@@ -1,6 +1,6 @@
 cask "awsssh" do
-  version "0.0.9"
-  sha256 "23e6a3c4a24afb6a5cf3469dce174eeca775ecbca29c4ab9ef7917a6e5a6209b"
+  version "1.0.0"
+  sha256 "f1880d89a125666d0a429121eeec4ccfb6adcefc64c0d9a289b46c224ed8d9b4"
 
   url "https://github.com/sratabix/awsssh/releases/download/v#{version}/Awsssh-#{version}.zip"
   name "Awsssh"
