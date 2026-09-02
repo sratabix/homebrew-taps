b89fb213de6e7c0fde9615c968be38c1c50ecac9
Sacha <s.ruhe@atabix.nl>
2026-09-02 07:16:08 +0200

    bump pingmenubar

--- Casks/pingmenubar.rb
+++ Casks/pingmenubar.rb
@@ -1,6 +1,6 @@
 cask "pingmenubar" do
-  version "0.0.2"
-  sha256 "8fd15cf5a73ad20fd3948c8282f579615eaf312f429c48996865669f61024ea1"
+  version "0.0.3"
+  sha256 "e0ee5d99427431b8a97a8644363ededf8f355cc17de5b5fd73463bb803aee237"
 
   url "https://github.com/sratabix/ping-menubar/releases/download/v#{version}/PingMenubar-#{version}.zip"
   name "PingMenubar"
